a636ea0d80c7cef65de66eb113ca72a2ced93516
Kirill Bogatikoff <kivi15penza@gmail.com>
2019-05-06 23:20:33 +0300

    JavaDoc improved

--- src/org/kllbff/magiclibrary/geometry/PointPosition.java
+++ src/org/kllbff/magiclibrary/geometry/PointPosition.java
@@ -1,7 +1,17 @@
 package org.kllbff.magiclibrary.geometry;
 
+/**
+ * <h4>Represents position of some point on the shape</h4>
+ * <p>Any point can locates in shape, on it's border or outside 
+ * 
+ * @author Kirill Bogatikov
+ * @version 1.0
+ * @since 1.0
+ */
 public enum PointPosition {
 	INSIDE, OUTSIDE,
+	/* used only for lines */
 	LEFT, RIGHT, ABOVE, BELOW,
+	/* used only for shapes */
 	ON_BORDER, IN_VERTEX
 }
