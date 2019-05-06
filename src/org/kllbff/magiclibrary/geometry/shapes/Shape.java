1c1dd899d0ed9a4b0d81c6aa00be0190b11a8abe
Kirill Bogatikoff <kivi15penza@gmail.com>
2019-05-07 02:39:32 +0300

    Shape interface added

---
+++ src/org/kllbff/magiclibrary/geometry/shapes/Shape.java
@@ -0,0 +1,21 @@
+package org.kllbff.magiclibrary.geometry.shapes;
+
+import org.kllbff.magiclibrary.geometry.Point;
+import org.kllbff.magiclibrary.geometry.PointPosition;
+import org.kllbff.magiclibrary.geometry.StraightLine;
+
+public interface Shape {
+	public Point[] getVertices();
+	public PointPosition getPointPosition(Point point);
+	public boolean contains(Point point);
+	public boolean contains(Shape other);
+	public Point getIntersection(StraightLine line);
+	public Shape getIntersectionArea(Shape other);
+	
+	public default boolean hasIntersection(StraightLine line) {
+		return getIntersection(line) != null;
+	}
+	public default boolean hasIntersectionArea(Shape other) {
+		return getIntersectionArea(other) != null;
+	}
+}
