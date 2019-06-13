2fc3442d8b1e09a6cbf61e3d0f13c9b5d48e06af
Kirill Bogatikov <kivi15penza@gmail.com>
2019-06-14 02:29:31 +0300

    Added document comment for VerticesComparator class

--- src/org/kllbff/magic/geometry/utils/VerticesComparator.java
+++ src/org/kllbff/magic/geometry/utils/VerticesComparator.java
@@ -4,16 +4,38 @@ import java.util.Comparator;
 
 import org.kllbff.magic.geometry.Point;
 
+/**
+ * <h3>Represents tool for sorting shape's vertices counter-clockwise</h3>
+ * 
+ * @author Kirill Bogatikov
+ * @version 1.0
+ * @since 0.0
+ */
 public class VerticesComparator implements Comparator<Point> {
+    /**
+     * Represents accuracy of angles comparing in sorting, in degrees
+     */
     public static final int DEGREES_ACCURACY = 3;
     
     private Point origin;
     private int accuracy; 
     
+    /**
+     * Constructs new VerticesComparator for shape with center at <i>origin</i> point 
+     * 
+     * @param origin center of shape, which vertices will be sorted by this comparator
+     */
     public VerticesComparator(Point origin) {
         this(origin, DEGREES_ACCURACY);
     }
     
+    /**
+     * Constructs new VerticesComparator for shape with center at <i>origin</i> point
+     * with specified comparing accuracy
+     * 
+     * @param origin center of shape, which vertices will be sorted by this comparator
+     * @param accuracy accuracy of comparing, in degrees
+     */
     public VerticesComparator(Point origin, int accuracy) {
         this.origin = origin;
         this.accuracy = accuracy;
