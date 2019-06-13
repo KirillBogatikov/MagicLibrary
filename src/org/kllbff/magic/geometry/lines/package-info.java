3d69128018bc0025c6e59d323243ef391e5c949f
Kirill Bogatikov <kivi15penza@gmail.com>
2019-06-14 02:09:41 +0300

    Added package-info files for each packages

---
+++ src/org/kllbff/magic/geometry/lines/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Contains lines implementations - 2D geometry objects, connecting 
+ * two points in plane. All lines must extends abstract class {@link Line}.<br>
+ * Package contains most popular in graphics lines:
+ * <ul>
+ *     <li>{@link BezierCurve} is quadratic Bezier curve line;</li>
+ *     <li>{@link StraightLine}, {@link LineSegment} is simple straight lines.</li>
+ * </ul>
+ */
+package org.kllbff.magic.geometry.lines;
---
+++ src/org/kllbff/magic/geometry/shapes/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Contains library's shapes implementations - 2D geometry objects, connecting 
+ * three or more points in plane. All shapes must extend abstract class {@link Shape}.<br>
+ * Package contains most popular in graphics lines:
+ * <ul>
+ *     <li>{@link Polygon} is free polygon containing three or more points;</li>
+ *     <li>{@link Rectangle} is a shape with four vertices and 90 degrees angle at every corner;</li>
+ *     <li>{@link Triangle} is a shape with three vertices and sum of angled at corners equals 180 degrees.</li>
+ * </ul>
+ */
+package org.kllbff.magic.geometry.shapes;
