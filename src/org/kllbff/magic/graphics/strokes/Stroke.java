695d6e948f4195361f26995e0a0627150e0b4bfe
Kirill Bogatikov <kivi15penza@gmail.com>
2019-07-06 13:09:34 +0300

    Added stroke abstract class

---
+++ src/org/kllbff/magic/graphics/strokes/Stroke.java
@@ -0,0 +1,61 @@
+package org.kllbff.magic.graphics.strokes;
+
+public abstract class Stroke {    
+    public static enum CapType {
+        BUTT,
+        ROUND,
+        SQUARE
+    }
+    
+    public static enum JoinType {
+        BEVEL,
+        SHARP,
+        ROUND
+    }
+    
+    protected int width;
+    protected long color;
+    protected CapType cap;
+    protected JoinType join;
+    
+    public Stroke(int width, long color, CapType cap, JoinType join) {
+        this.width = width;
+        this.color = color;
+        this.cap = cap;
+        this.join = join;
+    }
+    
+    public void setCapType(CapType cap) {
+        this.cap = cap;
+    }
+    
+    public CapType getCapType() {
+        return this.cap;
+    }
+    
+    public void setJoinType(JoinType join) {
+        this.join = join;
+    }
+    
+    public JoinType getJoinType() {
+        return this.join;
+    }
+    
+    public void setWidth(int width) {
+        this.width = width;
+    }
+    
+    public int getWidth() {
+        return width;
+    }
+    
+    public void setColor(long color) {
+        this.color = color;
+    }
+    
+    public long getColor() {
+        return color;
+    }
+    
+    public abstract float[] getDrawingPattern();
+}
