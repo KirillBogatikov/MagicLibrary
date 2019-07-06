bbb11d26b524798f83965ffe4203bfebe45f2ab0
Kirill Bogatikov <kivi15penza@gmail.com>
2019-07-06 22:31:02 +0300

    Corrected cos interpolation

--- src/org/kllbff/magic/interpolation/CosInterpolator.java
+++ src/org/kllbff/magic/interpolation/CosInterpolator.java
@@ -4,7 +4,7 @@ public class CosInterpolator implements Interpolator {
    
     @Override
     public double interpolate(double start, double end, double t1) {
-        t1 = (1 - Math.cos(t1 * Math.PI)) * 0.5;
+        t1 = (1 - Math.cos(t1 * Math.PI)) * 0.45;
         
         double t2 = (1 - t1);
         return start * t2 + end * t1;
