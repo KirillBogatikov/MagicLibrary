e873c51c3f908f04ded4b0f724f49fe65a18f086
Kirill Bogatikov <kivi15penza@gmail.com>
2019-07-06 13:02:13 +0300

    Added color spaces enumeration

---
+++ src/org/kllbff/magic/graphics/color/ColorSpace.java
@@ -0,0 +1,28 @@
+package org.kllbff.magic.graphics.color;
+
+public enum ColorSpace {
+    sRGB   (0),
+    CMYK   (1),
+    HSB    (2),
+    CIELAB (3),
+    XYZ    (4);
+    
+    private long flag;
+    
+    private ColorSpace(long flag) {
+        this.flag = flag;
+    }
+    
+    public long getFlag() {
+        return flag;
+    }
+    
+    public static ColorSpace forFlag(long flag) {
+        for(ColorSpace space : values()) {
+            if(space.flag == flag) {
+                return space;
+            }
+        }
+        return null;
+    }
+}
