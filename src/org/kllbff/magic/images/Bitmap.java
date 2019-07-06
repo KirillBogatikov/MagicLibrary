c2f4ba9838233400a419fad9f521c44fcff590eb
Kirill Bogatikov <kivi15penza@gmail.com>
2019-07-06 13:07:45 +0300

    Added Bitmap class. It represents raster image by array of long colors

---
+++ src/org/kllbff/magic/images/Bitmap.java
@@ -0,0 +1,45 @@
+package org.kllbff.magic.images;
+
+import java.util.Arrays;
+
+import org.kllbff.magic.exceptions.NotEnoughMemoryException;
+import org.kllbff.magic.utils.MemoryAllocator;
+
+public class Bitmap {
+    private long[] pixels;
+    private int width, height;
+    
+    public Bitmap(long[] pixels, int width, int height) {
+        this.pixels = pixels;
+        this.width = width;
+        this.height = height;
+    }
+    
+    public Bitmap(int width, int height) throws NotEnoughMemoryException {
+        this(MemoryAllocator.allocateLongArray(width * height), width, height);
+    }
+    
+    public int getWidth() {
+        return width;
+    }
+    
+    public int getHeight() {
+        return height;
+    }
+    
+    public long getPixel(int x, int y) {
+        return pixels[y * width + x];
+    }
+    
+    public void setPixel(int x, int y, long color) {
+        pixels[y * width + x] = color;
+    }
+    
+    public long[] grabPixels() {
+        return pixels;
+    }
+    
+    public Bitmap copy(int width, int height) throws NotEnoughMemoryException {
+        return new Bitmap(Arrays.copyOf(pixels, width * height), width, height);
+    }
+}
