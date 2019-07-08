fe361b683bfcbaaa6971fad2c49f6206bce5b122
Kirill Bogatikov <kivi15penza@gmail.com>
2019-07-08 03:07:10 +0300

    Added exception for font system

---
+++ src/org/kllbff/magic/exceptions/FontException.java
@@ -0,0 +1,9 @@
+package org.kllbff.magic.exceptions;
+
+public class FontException extends ParsingException {
+    private static final long serialVersionUID = 4496477565271740572L;
+
+    public FontException(String message) {
+        super(message);
+    }
+}
