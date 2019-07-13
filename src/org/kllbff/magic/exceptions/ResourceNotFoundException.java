ec6f549a1abc2cec2b91869ee2a825cfd237632a
Kirill Bogatikov <kivi15penza@gmail.com>
2019-07-13 23:46:20 +0300

    Added new exception for Resources class

---
+++ src/org/kllbff/magic/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,15 @@
+package org.kllbff.magic.exceptions;
+
+public class ResourceNotFoundException extends Exception {
+    private static final long serialVersionUID = -7166805606022798320L;
+
+    public ResourceNotFoundException() {}
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(Throwable t) {
+        super(t);
+    }
+}
