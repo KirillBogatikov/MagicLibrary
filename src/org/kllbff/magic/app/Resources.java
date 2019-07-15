ca5ca5bc235e7a8e326d6bcfd1f1ffe87a3ab93d
Kirill Bogatikov <kivi15penza@gmail.com>
2019-07-15 23:15:58 +0300

    Resources rewroten for work with basic attributes and theme's aliases
    Now Resources provides access to data from two sources. Basic, not
    linked attributes. It is all known for application resources from /res/
    directory. Access for it provides by theme aliases described in
    /res/styles/*.json or /res/styles/*.xml. You can store many resources in
    /res/ directory: drawables, values, but provides for application only
    some, described by theme. You must create at least one theme

--- src/org/kllbff/magic/app/Resources.java
+++ src/org/kllbff/magic/app/Resources.java
@@ -1,5 +1,9 @@
 package org.kllbff.magic.app;
 
+import static org.kllbff.magic.styling.AttributeType.DIMENSION;
+import static org.kllbff.magic.styling.AttributeType.DRAWABLE;
+import static org.kllbff.magic.styling.AttributeType.STRING;
+
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -7,7 +11,6 @@ import java.io.InputStreamReader;
 
 import org.kllbff.magic.exceptions.ParsingException;
 import org.kllbff.magic.exceptions.ResourceNotFoundException;
-import org.kllbff.magic.graphics.color.Color;
 import org.kllbff.magic.graphics.drawable.BitmapDrawable;
 import org.kllbff.magic.graphics.drawable.Drawable;
 import org.kllbff.magic.graphics.drawable.NinePatchDrawable;
@@ -22,136 +25,119 @@ import org.kllbff.magic.styling.StateList;
 import org.xmlpull.v1.XmlPullParserException;
 
 public class Resources {
-    private AttributeSet attributes;
+    private AttributeSet basicAttributes;
+    private AttributeSet themeAliases;
     
-    public Resources(AttributeSet attributes) throws XmlPullParserException {
-        this.attributes = attributes;
+    public Resources(AttributeSet basic, AttributeSet aliases) throws XmlPullParserException {
+        this.basicAttributes = basic;
+        this.themeAliases = aliases;
+    }
+    
+    public void applyTheme(AttributeSet aliases) {
+        this.themeAliases = aliases;
     }
     
     private void throwParsingException(String target, Attribute attr) throws ParsingException {
         throw new ParsingException("Failed to get " + target + " from attribute '" + attr.getName() + "': type '" + attr.getType() + "' is incompatible ");
     }
     
+    private void checkResource(String target, String name) throws ResourceNotFoundException {
+        if(!basicAttributes.has(name) && !themeAliases.has(name)) {
+            throw new ResourceNotFoundException(target + " '" + name + "' not found");
+        }
+    }
+    
     public Drawable getDrawable(String name) throws ResourceNotFoundException {
         name = name.replace("@drawable/", "");
         
-        if(!attributes.has(name)) {
-            throw new ResourceNotFoundException("Drawable " + name + " not found");
+        if(!basicAttributes.has(name)) {
+            return getDrawable(themeAliases.getValue(name));
         }
-        
-        Attribute attr = attributes.get(name);
-        if(attr.is(AttributeType.DRAWABLE)) {
-            return attr.get();
-        } 
-        if(attr.is(AttributeType.STRING)) {
-            Drawable drawable;
-            String resPath = "res/drawable/" + attr.get();
-            
-            try(InputStream input = new BufferedInputStream(new FileInputStream(resPath))) {
-                if(resPath.endsWith(".xml")) {
-                    XmlDrawableParser xmlDrawableParser = new XmlDrawableParser(this);
-                    drawable = xmlDrawableParser.parseResource(new InputStreamReader(input));
-                } else if(resPath.endsWith(".json")) {
-                    JsonDrawableParser jsonDrawableParser = new JsonDrawableParser(this);
-                    drawable = jsonDrawableParser.parseResource(new InputStreamReader(input));
-                } else {
-                    Bitmap bitmap = BitmapFactory.readBitmap(input, null);
-                    if(resPath.endsWith(".9.png")) {
-                        drawable = new NinePatchDrawable(bitmap);
+
+        checkResource("Drawable", name);
+        Attribute attr = basicAttributes.get(name);
+        switch(attr.getType()) {
+            case DRAWABLE: 
+                return attr.get();
+            case STRING: {
+                Drawable drawable;
+                String resPath = "res/drawable/" + attr.get();
+                
+                try(InputStream input = new BufferedInputStream(new FileInputStream(resPath))) {
+                    if(resPath.endsWith(".xml")) {
+                        XmlDrawableParser xmlDrawableParser = new XmlDrawableParser(this);
+                        drawable = xmlDrawableParser.parseResource(new InputStreamReader(input));
+                    } else if(resPath.endsWith(".json")) {
+                        JsonDrawableParser jsonDrawableParser = new JsonDrawableParser(this);
+                        drawable = jsonDrawableParser.parseResource(new InputStreamReader(input));
+                    } else {
+                        Bitmap bitmap = BitmapFactory.readBitmap(input, null);
+                        if(resPath.endsWith(".9.png")) {
+                            drawable = new NinePatchDrawable(bitmap);
+                        }
+                        drawable = new BitmapDrawable(bitmap);
                     }
-                    drawable = new BitmapDrawable(bitmap);
+                    attr.set(drawable, DRAWABLE);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    throw new ParsingException(e);
                 }
-                attr.set(drawable, AttributeType.DRAWABLE);
-            } catch (Exception e) {
-                e.printStackTrace();
-                throw new ParsingException(e);
+                return drawable;
             }
-            return drawable;
+            default:
+                throwParsingException("drawable", attr);
         }
-        throwParsingException("drawable", attr);
         return null;
     }
     
     public long getColor(String name) throws ResourceNotFoundException {
         name = name.replace("@color/", "");
 
-        if(!attributes.has(name)) {
-            throw new ResourceNotFoundException("Color " + name + " not found");
+        if(!basicAttributes.has(name)) {
+            return getColor(themeAliases.getValue(name));
         }
+
+        checkResource("Color", name);
         
-        Attribute attr = attributes.get(name);
-        if(attr.is(AttributeType.COLOR)) {
-            return attr.get();
-        } 
-        if(attr.is(AttributeType.STRING)) {
-            String value = attr.get();
-            long color;
-            if(value.startsWith("#")) {
-                color = Color.hex(value);
-            } else {
-                color = getColor(attr.get());
-            }
-            attr.set(color, AttributeType.COLOR);
-            return color;
+        Attribute attr = basicAttributes.get(name);
+        switch(attr.getType()) {
+            case COLOR: 
+                return attr.get();
+            default:
+                throwParsingException("color", attr);
         }
-        throwParsingException("color", attr);
         return 0L;
     }
     
     public double getDimension(String name) throws ResourceNotFoundException {
         name = name.replace("@dimen/", "");
 
-        if(!attributes.has(name)) {
-            throw new ResourceNotFoundException("Dimension " + name + " not found");
+        if(!basicAttributes.has(name)) {
+            return getDimension(themeAliases.getValue(name));
         }
-        
-        Attribute attr = attributes.get(name);
-        if(attr.is(AttributeType.DIMENSION)) {
-            return attr.get();
-        } 
-        if(attr.is(AttributeType.STRING)) {
-            String value = attr.get();
-            double dimen;
-            if(value.startsWith("@")) {
-                dimen = getColor(attr.get());
-            } else {
-                dimen = Double.valueOf(value);
-                /**
-                 *  TODO add conversions from dp/px/sp/pt
-                 */
-            }
-            attr.set(dimen, AttributeType.DIMENSION);
-            return dimen;
-        }
-        throwParsingException("color", attr);
-        return 0L;
-    }
-    
-    public String getLayout(String name) throws ResourceNotFoundException {
-        name = name.replace("@layout/", "");
 
-        if(!attributes.has(name)) {
-            throw new ResourceNotFoundException("Layout " + name + " not found");
-        }
+        checkResource("Dimension", name);
         
-        return attributes.getValue(name);
+        Attribute attr = basicAttributes.get(name);
+        if(attr.is(DIMENSION)) {
+            return attr.get();
+        }
+        throwParsingException("dimension", attr);
+        return 0L;
     }
     
     public String getString(String name) throws ResourceNotFoundException {
         name = name.replace("@string/", "");
 
-        if(!attributes.has(name)) {
-            throw new ResourceNotFoundException("String " + name + " not found");
+        if(!basicAttributes.has(name)) {
+            return getString(themeAliases.getValue(name));
         }
+
+        checkResource("String", name);
         
-        Attribute attr = attributes.get(name);
-        if(attr.is(AttributeType.STRING)) {
-            String s = attr.get();
-            if(s.startsWith("@")) {
-                s = getString(s);
-                attr.set(s, AttributeType.STRING);
-            }
-            return s;
+        Attribute attr = basicAttributes.get(name);
+        if(attr.is(STRING)) {
+            return attr.get();
         }
         throwParsingException("string", attr);
         return null;
@@ -160,20 +146,23 @@ public class Resources {
     public StateList<?> getStateList(String name) throws ResourceNotFoundException {
         name = name.replaceAll("@(drawable|color)/", "");
 
-        if(!attributes.has(name)) {
-            throw new ResourceNotFoundException("StateList " + name + " not found");
+        if(!basicAttributes.has(name)) {
+            return getStateList(themeAliases.getValue(name));
         }
+
+        checkResource("StateList", name);
         
-        Attribute attr = attributes.get(name);
-        if(attr.is(AttributeType.STATE_LIST)) {
-            return attr.get();
+        Attribute attr = basicAttributes.get(name);
+        switch(attr.getType()) {
+            case STATE_LIST:
+                return attr.get();
+            case STRING:
+                StateList<?> list = getStateList(attr.get());
+                attr.set(list, AttributeType.STATE_LIST);
+                return list;
+            default:
+                throwParsingException("state list", attr);
         }
-        if(attr.is(AttributeType.STRING)) {
-            StateList<?> list = getStateList(attr.get());
-            attr.set(list, AttributeType.STATE_LIST);
-            return list;
-        }
-        throwParsingException("state list", attr);
         return null;
-    }
+    } 
 }
