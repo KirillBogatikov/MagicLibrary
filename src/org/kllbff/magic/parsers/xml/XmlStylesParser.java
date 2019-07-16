6451f6019f29fa4d559f8a1d99fe68d4fb8ab36e
Kirill Bogatikov <kivi15penza@gmail.com>
2019-07-16 18:25:36 +0300

    Added Styles and Values Parsers for XML

---
+++ src/org/kllbff/magic/parsers/xml/XmlStylesParser.java
@@ -0,0 +1,73 @@
+package org.kllbff.magic.parsers.xml;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kllbff.magic.exceptions.ParsingException;
+import org.kllbff.magic.styling.Attribute;
+import org.kllbff.magic.styling.AttributeSet;
+import org.kllbff.magic.styling.AttributeType;
+import org.kllbff.magic.styling.Theme;
+import org.xmlpull.v1.XmlPullParserException;
+
+public class XmlStylesParser extends XmlAbstractParser<List<Theme>> {
+
+    public XmlStylesParser() throws XmlPullParserException {
+        super(null);
+        this.linksAllowed = false;
+    }
+    
+    private Theme parseTheme() throws XmlPullParserException, IOException {
+        checkAttr("name", "theme");
+        String name = getAttr("name", null);
+        AttributeSet attributes = new AttributeSet();
+        
+        String tag;
+        while((tag = nextTag("theme")) != null) {
+            checkAttr("link", "theme alias");
+            
+            String link = getAttr("link", null);
+            
+            AttributeType attrType;
+            switch(tag) {
+                case "drawable": attrType = AttributeType.DRAWABLE; break;
+                case "color": attrType = AttributeType.COLOR; break;
+                case "dimen": attrType = AttributeType.DIMENSION; break;
+                case "state-list": attrType = AttributeType.STATE_LIST; break;
+                case "string": attrType = AttributeType.STRING; break;
+                default:
+                    throw new RuntimeException("Cannot parse tag <" + tag + "> as theme alias"); 
+            }
+            
+            String value = getAttr("source", null);
+            if(value == null) {
+                value = xpp.nextText();
+            }
+            
+            attributes.add(new Attribute(attrType, link, value));
+        }
+        
+        return new Theme(name, attributes);
+    }
+
+    @Override
+    public List<Theme> parseResource(Reader reader) throws IOException, XmlPullParserException {
+        xpp.setInput(reader);
+        List<Theme> list = new ArrayList<>();
+        
+        if(!nextTag("").equals("resources")) {
+            throw new ParsingException("Values file must be started with <resources> tag");
+        }
+        
+        String tag;
+        while((tag = nextTag("resources")) != null) {
+            System.out.println(tag);
+            list.add(parseTheme());
+        }
+        
+        return list;
+    }
+
+}
---
+++ src/org/kllbff/magic/parsers/xml/XmlValuesParser.java
@@ -0,0 +1,51 @@
+package org.kllbff.magic.parsers.xml;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import org.kllbff.magic.exceptions.ParsingException;
+import org.kllbff.magic.exceptions.ResourceNotFoundException;
+import org.kllbff.magic.styling.Attribute;
+import org.kllbff.magic.styling.AttributeSet;
+import org.kllbff.magic.styling.AttributeType;
+import org.xmlpull.v1.XmlPullParserException;
+
+public class XmlValuesParser extends XmlAbstractParser<AttributeSet> {
+
+    public XmlValuesParser() throws XmlPullParserException {
+        super(null);
+        this.linksAllowed = false;
+    }
+
+    @Override
+    public AttributeSet parseResource(Reader reader) throws IOException, XmlPullParserException, ResourceNotFoundException {
+        xpp.setInput(reader);
+        
+        if(!nextTag("").equals("resources")) {
+            throw new ParsingException("Values file must be started with <resources> tag");
+        }
+        
+        AttributeSet attributes = new AttributeSet();
+        String tag;
+        while((tag = nextTag("resources")) != null) {
+            checkAttr("name", "resources item");
+            
+            String name = getAttr("name", null);
+            String value = getAttr("value", null);
+            if(value == null) {
+                value = xpp.nextText();
+            }
+            
+            switch(tag) {
+                case "dimen": attributes.add(new Attribute(AttributeType.DIMENSION, name, toDimen(value))); break;
+                case "color": attributes.add(new Attribute(AttributeType.COLOR, name, toColor(value))); break;
+                case "string": attributes.add(new Attribute(AttributeType.STRING, name, value)); break;
+                default:
+                    throw new RuntimeException("Failed to parse unknown resource tag <" + tag + ">");
+            }
+        }
+        
+        return attributes;
+    }
+
+}
