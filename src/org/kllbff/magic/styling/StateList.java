743e927dfad69488d9a403465304721daac31f92
Kirill Bogatikov <kivi15penza@gmail.com>
2019-07-19 02:20:07 +0300

    Rewrited StateList class: added masks and states constants

--- src/org/kllbff/magic/styling/StateList.java
+++ src/org/kllbff/magic/styling/StateList.java
@@ -1,47 +1,73 @@
 package org.kllbff.magic.styling;
 
+import java.util.ArrayList;
+
 public class StateList<V> {
-    private V disabledChecked, disabledUnchecked;
-    private V pressedChecked, pressedUnchecked;
-    private V focusedChecked, focusedUnchecked;
-    private V checked, unchecked;
+    public static final int STATE_DISABLED  = 0x00;
+    public static final int STATE_ENABLED   = 0x01;
+    public static final int STATE_HOVERED   = 0x02;
+    public static final int STATE_FOCUSED   = 0x04;
+    public static final int STATE_SELECTED  = 0x08;
+    public static final int STATE_PRESSED   = 0x10;
+    public static final int STATE_CHECKED   = 0x20;
     
-    public StateList(V dc, V du, V pc, V pu, V fc, V fu, V c, V u) {
-        this.disabledChecked = dc;
-        this.disabledUnchecked = du;
-        this.pressedChecked = pc;
-        this.pressedUnchecked = pu;
-        this.focusedChecked = fc;
-        this.focusedUnchecked = fu;
-        this.checked = c;
-        this.unchecked = u;
+    public static final int MASK_DISABLED   = 0b11111111111111111111111111111110;
+    public static final int MASK_UNHOVERED  = 0b11111111111111111111111111111101;
+    public static final int MASK_UNFOCUSED  = 0b11111111111111111111111111111011;
+    public static final int MASK_UNSELECTED = 0b11111111111111111111111111110111;
+    public static final int MASK_UNPRESSED  = 0b11111111111111111111111111101111;
+    public static final int MASK_UNCHECKED  = 0b11111111111111111111111111011111;
+    
+    
+    private ArrayList<Integer> states;
+    private ArrayList<V> values;
+    
+    public StateList(int[] states, V[] values) {
+        this.states = new ArrayList<>();
+        this.values = new ArrayList<>();
+        for(int i = 0; i < states.length; i++) {
+            addState(states[i], values[i]);
+        }
     }
     
-    public V getDisabled(boolean checked) {
-        if(checked && disabledChecked != null) {
-            return disabledChecked;
-        }
-        return disabledUnchecked;
+    public void addState(int state, V value) {
+        this.states.add(state);
+        this.values.add(value);
     }
     
-    public V getPressed(boolean checked) {
-        if(checked && pressedChecked != null) {
-            return pressedChecked;
-        }
-        return pressedUnchecked;
+    private int bit(int n, int i) {
+        return (n >> i) & 1;
     }
     
-    public V getFocused(boolean checked) {
-        if(checked && focusedChecked != null) {
-            return focusedChecked;
+    private int similarity(int firstNumber, int secondNumber) {
+        if(bit(firstNumber, 0) != bit(secondNumber, 0)) {
+            return 0;
         }
-        return focusedUnchecked;
+        
+        int similarity = 0;
+        for(int bitOffset = 1; bitOffset < 32; bitOffset++) {
+            similarity += bit(firstNumber, bitOffset) == bit(secondNumber, bitOffset) ? bitOffset : 0;
+        }
+        return similarity;
     }
     
-    public V getDefault(boolean checked) {
-        if(checked && this.checked != null) {
-            return this.checked;
+    public V getMostSimilar(int state) {
+        int index = -1;
+        int maxSimilarity = 0;
+        for(int i = 0; i < states.size(); i++) {
+            int currentSimilarity = similarity(states.get(i), state);
+            if(currentSimilarity > maxSimilarity) {
+                index = i;
+                maxSimilarity = currentSimilarity;
+            }
         }
-        return unchecked;
+        if(index > -1) {
+            return values.get(index);
+        }
+        return null;
+    }
+    
+    public V get(int state) {
+        return values.get(state);
     }
 }
