82b99710fded90c896ca3ab06dff956afd512e34
Kirill Bogatikov <kivi15penza@gmail.com>
2019-07-21 05:29:06 +0300

    boolean secondary argument replaced with MouseEvent

--- src/org/kllbff/magic/view/OnClickListener.java
+++ src/org/kllbff/magic/view/OnClickListener.java
@@ -1,5 +1,7 @@
 package org.kllbff.magic.view;
 
+import org.kllbff.magic.event.MouseEvent;
+
 public interface OnClickListener {
-    public void onClick(View view, boolean secondary);
+    public void onClick(View view, MouseEvent event);
 }
